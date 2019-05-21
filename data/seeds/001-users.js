f5acedf1527dea5f8b1b626b61b7a7a72db1885a
Tristan Grovender <therealtristangrovender@gmail.com>
2019-05-21 17:33:38 -0500

    ran seeds

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,13 +1,42 @@
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex("users")
-    .truncate()
-    .then(function() {
-      // Inserts seed entries
-      return knex("users").insert([
-        { id: 1, name: "Tristan", email: "tristan.grovender@gmail.com", password: "pass" },
-        { id: 2, name: "Azra", email: "azra.panjwani@gmail.com", password: "pass" },
-        { id: 3, name: "Hung", email: "hung.pham@gmail.com", password: "pass" },
-      ]);
-    });
-};
+exports.seed = function (knex, Promise) {
+  const hashPassword = require('../../helpers/hashPassword');
+  const hash = hashPassword('pass');
+ 
+    // Deletes ALL existing entries
+    return knex('users').del()
+      .then(function () {
+        // Inserts seed entries
+        return knex('users').insert([
+          {
+            id: 1,
+            username: 'Tristan',
+            password: hash,            
+            name: 'Tristan Grovender',
+            email: 'tristangrovender@gmail.com',
+            location: 'Minneapolis, Minnesota',
+            bio: 'I\'m a bio!!',
+            professional: false,
+          },
+          {
+            id: 2,
+            username: 'Azra',
+            password: hash,
+            name: 'Azra Panjwani',            
+            email: 'azrapanjwani@gmail.com',
+            location: 'San Francisco, California',
+            bio: 'I\'m a bio!!',
+            professional: false,
+          },
+          {
+            id: 3,
+            username: 'Hung',
+            password: hash,            
+            name: 'Hung Pham',
+            email: 'hungpham@gmail.com',
+            location: 'Washington, D.C.',
+            bio: 'I\'m a bio!!',
+            professional: false,
+          },
+        ]);
+      });
+  };
